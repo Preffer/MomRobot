5d8f29c64de793077e7d34b7655cc29d976679a2
Yuzo <hyzgog@gmail.com>
2014-08-04 08:44:00 +0800

    bug version

--- excelreader.h
+++ excelreader.h
@@ -18,9 +18,14 @@ private:
     void getKey();
     void getValue();
     void pushValue();
+    void checkNewMonth();
+
+    QSharedPointer<QVariant> pack(QSharedPointer<QStringList> list);
+    //abort to build an unpack function because many additional works done in its iteration
 
     int keyIndex;
     int valueIndex;
+    int dateIndex;
     int dataStart;
     int dataEnd;
 
@@ -43,7 +48,9 @@ private:
     static int keyLength;
     static QString keyName;
     static QString valueName;
+    static QString dateName;
     static QString dataPrefix;
+    static int newMonthBound;
 };
 
 #endif // EXCELREADER_H
